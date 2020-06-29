b57b50e04bd5b0b7012c991294b071151cc7ce20
Nasuru <nasuruf@gmail.com>
2020-06-28 19:39:39 -0600

    Updates

---
+++ precache-manifest.e048408a7d2d0122fcbd2238f5d1a2d2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "036806bcf77d213a3538cd4f52b5c6bc",
+    "url": "/visahackathon/index.html"
+  },
+  {
+    "revision": "dc17a5420fbce6ff5cc4",
+    "url": "/visahackathon/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f001be4e567227332e88",
+    "url": "/visahackathon/static/css/main.01b99346.chunk.css"
+  },
+  {
+    "revision": "dc17a5420fbce6ff5cc4",
+    "url": "/visahackathon/static/js/2.ef41a290.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/visahackathon/static/js/2.ef41a290.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f001be4e567227332e88",
+    "url": "/visahackathon/static/js/main.b7b78abd.chunk.js"
+  },
+  {
+    "revision": "373f0657adccbdb05166",
+    "url": "/visahackathon/static/js/runtime-main.647438cd.js"
+  },
+  {
+    "revision": "463d792e23eda7f1b2a038a074d098e0",
+    "url": "/visahackathon/static/media/pic1.463d792e.jpg"
+  },
+  {
+    "revision": "ae0c07b68b926f1c0052376d4b00832f",
+    "url": "/visahackathon/static/media/pic2.ae0c07b6.png"
+  },
+  {
+    "revision": "aa9337f7355b4df54b2179c69e22b689",
+    "url": "/visahackathon/static/media/pic3.aa9337f7.jpg"
+  },
+  {
+    "revision": "b8be50c7b9283aa0e1172c9de371a986",
+    "url": "/visahackathon/static/media/sampleCard.b8be50c7.png"
+  }
+]);
